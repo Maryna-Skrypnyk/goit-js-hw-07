028e5fef93bbe8652e88195166c194394a9626ec
Oleg-Skrypnyk <olegaskryp@gmail.com>
2021-04-20 18:22:48 +0300

    Done HW 3

--- js/task-03.js
+++ js/task-03.js
@@ -16,10 +16,64 @@ const images = [
   },
 ];
 
-const imagesEl = document.querySelector("#gallery");
+// Як повинна виглядати розмітка
 
-const itemsEl = document.createElement("li");
-const imgEl = document.createElement("img");
+/* <ul id="gallery" class="gallery">
+    <li class="item">
+        <img class="image" src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt = "White and Black Long Fur Cat"/> 
+    </li>
+    <li class="item">
+        <img class="image" src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt = 'Orange and White Koi Fish Near Yellow Koi Fish'/> 
+    </li>
+    <li class="item">
+        <img class="image" src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt = 'Group of Horses Running'/> 
+    </li>
+    ...
+</ul> */
 
-itemsEl.append(imgEl);
 
+// 1 Спосіб (через createElement та append)
+
+// const galleryEl = document.querySelector("#gallery");
+// galleryEl.classList.add("gallery");
+
+// const makeImagesItems = ({url, alt}) => {
+//   const itemsEl = document.createElement("li");
+//   itemsEl.classList.add("item");
+
+//   const imgEl = document.createElement("img");
+//   imgEl.classList.add("image");
+//   imgEl.src = url;
+//   imgEl.alt = alt;
+//   imgEl.width = 600;
+
+//   itemsEl.append(imgEl);
+//   // galleryEl.append(itemsEl);
+  
+//   return itemsEl;
+// };
+
+// const imagesMarkup = images.map(makeImagesItems);
+// galleryEl.append(...imagesMarkup);
+
+
+// 2 Спосіб (через insertAdjacentHTML())
+
+const galleryEl = document.querySelector("#gallery");
+galleryEl.classList.add("gallery");
+
+const makeImagesItems = ({url, alt}) => {
+  return `
+  <li class="item">
+    <img class="image"
+        src = ${url} 
+        alt = ${alt}
+        /> 
+  </li>`;
+};
+
+const imagesMarkup = images.map(makeImagesItems).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+console.log(galleryEl);
