546866600f28a6d999d65b1aadfca71f24bc7349
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-04-26 11:23:06 +0300

    HW 5, 8 corrects

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,27 @@
+// const refs = {
+//   input: document.querySelector('#name-input'),
+//   output: document.querySelector('#name-output'),
+// };
+
+// const onInputChange = event => {
+//   refs.output.textContent = event.currentTarget.value;
+
+//   if (event.currentTarget.value === '') {
+//     refs.output.textContent = 'незнайомець';
+//   }
+// };
+
+// refs.input.addEventListener('input', onInputChange);
+
 const refs = {
-    input: document.querySelector("#name-input"),
-    output: document.querySelector("#name-output"),
+  input: document.querySelector('#name-input'),
+  output: document.querySelector('#name-output'),
 };
 
 const onInputChange = event => {
-    refs.output.textContent = event.currentTarget.value;
+  event.currentTarget.value !== ''
+    ? (refs.output.textContent = event.currentTarget.value)
+    : (refs.output.textContent = 'незнайомець');
 };
 
 refs.input.addEventListener('input', onInputChange);
